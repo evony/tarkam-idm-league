9885d91dc86a39a05db4b7fa83f8926c4fbe4023
Z User <z@container>
2026-04-20 05:39:51 +0000

    fce5c070-6e79-421c-b5a0-e63edf187250

--- src/components/idm/landing/hero-section.tsx
+++ src/components/idm/landing/hero-section.tsx
@@ -68,7 +68,7 @@ export function HeroSection({
               <div className="absolute inset-0 w-full h-full overflow-hidden">
                 <iframe
                   src={`https://www.youtube.com/embed/${ytId}?autoplay=1&mute=1&loop=1&playlist=${ytId}&controls=0&showinfo=0&rel=0&modestbranding=1&playsinline=1&enablejsapi=1`}
-                  className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[150%] h-[150%] min-w-full min-h-full border-0 pointer-events-none"
+                  className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[400%] h-[400%] min-w-full min-h-full border-0 pointer-events-none"
                   allow="autoplay; encrypted-media"
                   allowFullScreen
                   aria-hidden="true"
